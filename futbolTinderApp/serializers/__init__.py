cf0c01f95e0618b7d5be5edc75de8d180c247d69
Juan Rueda González <64388719+RuedaGJuan@users.noreply.github.com>
2021-10-30 13:43:26 -0500

    Create __init__.py

---
+++ futbolTinderApp/serializers/__init__.py
@@ -0,0 +1,4 @@
+from .userSerializer import UserSerializer
+from .equipoSerializer import EquipoSerializer
+from .convocatoriaSerializer import ConvocatoriaSerializer
+from .jugadorSerializer import JugadorSerializer
